44095f204898302f16e679b2cc2bcff054ddcbda
Francisco Li <liruizfrancisco@gmail.com>
2026-09-03 10:50:44 -0600

    Version final

--- Theme_Park/Program.cs
+++ Theme_Park/Program.cs
@@ -4,7 +4,7 @@
     {
        //Pruebas de instancia del funcionamiento general
        
-       //Prueba: Clase padre atraccion con hija Montaña rusa
+       /*//Prueba: Clase padre atraccion con hija Montaña rusa
        MontanaRusa montana = new MontanaRusa("Montaña Rusa", 24, 20, true);
         montana.ObtenerInfo();
         montana.Operar();
@@ -20,7 +20,7 @@
         montana.SetCapacidadMaxima(-5); 
         montana.SetAlturaMax(18);
         montana.SetDeCabeza(false);
-        montana.ObtenerInfo();
+        montana.ObtenerInfo();*/
 
         //Prueba Montaña rusa vr, herencia nivel 3, hija de montaña rusa
         MontanaRusaVR vr = new MontanaRusaVR("Realidad Alterna", 8, 15, false, "Aventura Espacial");
@@ -35,7 +35,7 @@
         vr.RecargarLentes();
         vr.Operar(); 
 
-        //Pruebas Carrusel, hija de atraccion, otra rama de herencia
+       /* //Pruebas Carrusel, hija de atraccion, otra rama de herencia
         Carrusel carruselNuevo = new Carrusel("Carrusel Animalitos", 20, 10, 120);
         carruselNuevo.ObtenerInfo();
         Console.WriteLine(carruselNuevo.CumpleEstatura(134)); 
@@ -58,20 +58,20 @@
         juan.MostrarInfo();
 
         juan.PuedeUsarAtraccion(montana); //Con pase especial, no gasta tiquete
-        juan.MostrarInfo();
+        juan.MostrarInfo();*/
 
-        // atracción en mantenimiento bloquea el acceso, aunque tenga pase especial
+        /*// atracción en mantenimiento bloquea el acceso, aunque tenga pase especial
         montana.DarMantenimiento();
         juan.PuedeUsarAtraccion(montana); 
 
-        montana.FinalizarMantenimiento(); 
+        montana.FinalizarMantenimiento(); */
         
-        //Pruebas clase Tiquete
+        /*//Pruebas clase Tiquete
         Tiquete tiqueteMontana = new Tiquete(montana);
         tiqueteMontana.ObtenerInfo();
 
         tiqueteMontana.UsarTiquete(); //Primera vez: se marca como usado
         tiqueteMontana.UsarTiquete(); //Segunda vez: debería avisar que ya se usó
-        tiqueteMontana.ObtenerInfo();
+        tiqueteMontana.ObtenerInfo();*/
     }
 }
